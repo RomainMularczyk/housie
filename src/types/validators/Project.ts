e975432249257639a802637502ff06d12add843d
Romain Mularczyk <romain.mularczyk@gmail.com>
2025-08-02 13:10:11 +0200

    formatting

--- src/types/validators/Project.ts
+++ src/types/validators/Project.ts
@@ -2,25 +2,28 @@ import { z } from 'zod';
 
 const CreateProjectSchema = z.object({
   name: z
-    .string({ message: 'Project\'s name must be a string.' })
-    .min(1, { message: 'Project\'s must at least contain one character.' }),
-  userId: z
-    .string({ message: 'Project\'s user ID must be a string.' })
-    .uuid({ message: 'Project\'s user ID must be a valid UUID.' }),
+    .string({ message: "Project's name must be a string." })
+    .min(1, { message: "Project's must at least contain one character." }),
+  userId: z.string({ message: "Project's user ID must be a string." }),
 });
 
 const SelectProjectSchema = z.object({
   id: z
-    .string({ message: 'Project\'s ID must be a string.' })
-    .uuid({ message: 'Project\'s ID must be a valid UUID.' }),
+    .string({ message: "Project's ID must be a string." })
+    .uuid({ message: "Project's ID must be a valid UUID." }),
   name: z
-    .string({ message: 'Project\'s name must be a string.' })
-    .min(1, { message: 'Project\'s must at least contain one character.' }),
-  userId: z
-    .string({ message: 'Project\'s user ID must be a string.' })
-    .uuid({ message: 'Project\'s user ID must be a valid UUID.' }),
+    .string({ message: "Project's name must be a string." })
+    .min(1, { message: "Project's must at least contain one character." }),
+  userId: z.string({ message: "Project's user ID must be a string." }),
 });
 
-const UpdateProjectSchema = z.object({});
+const UpdateProjectSchema = z
+  .object({
+    name: z
+      .string({ message: "Project's name must be a string." })
+      .min(1, { message: "Project's must at least contain one character." }),
+    userId: z.string({ message: "Project's user ID must be a string." }),
+  })
+  .partial();
 
 export { CreateProjectSchema, SelectProjectSchema, UpdateProjectSchema };
